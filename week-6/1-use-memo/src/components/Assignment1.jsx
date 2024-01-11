8c1d2b241c5b08acc4b1a23df6f3bdd603cb3563
Harpreet Singh <szinghh@gmail.com>
2024-01-11 19:10:20 +0100

    started week6

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { useMemo } from 'react';
+import { useState } from 'react';
 
-// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input.
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
-export function Assignment1() {
-    const [input, setInput] = useState(0);
-    // Your solution starts here
-    const expensiveValue = 0; 
-    // Your solution ends here
+export default function Assignment1() {
+  const [input, setInput] = useState(0);
+  // Your solution starts here
+  const expensiveValue = useMemo(() => {
+    const factorial = (num) => {
+      if (num === 1) {
+        return 1;
+      } else if (num < 0) {
+        return -1;
+      } else if (num === 0) {
+        return 0;
+      } else {
+        return num * factorial(num - 1);
+      }
+    };
 
-    return (
-        <div>
-            <input 
-                type="number" 
-                value={input} 
-                onChange={(e) => setInput(Number(e.target.value))} 
-            />
-            <p>Calculated Value: {expensiveValue}</p>
-        </div>
-    );
+    return factorial(input);
+  }, [input]);
+  // Your solution ends here
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(Number(e.target.value))}
+      />
+      <p>Calculated Value: {expensiveValue}</p>
+    </div>
+  );
 }
--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from 'react';
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
-const Assignment3 = () => {
+ const Assignment3 = () => {
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
