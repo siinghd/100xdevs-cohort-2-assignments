ef23339a076f157aa2d4dfad8528cd397dde6fc6
Harpreet Singh <szinghh@gmail.com>
2024-01-17 16:42:14 +0100

    done 02

--- week-7/bg-changer/src/App.tsx
+++ week-7/bg-changer/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [backgroundColor, setBackgroundColor] = useState('white');
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  const changeBackground = () => {
+    const newColor = getRandomColor();
+    setBackgroundColor(newColor);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        backgroundColor: backgroundColor,
+        minHeight: '100vh',
+        transition: 'background-color 0.5s',
+        width: '100dvw',
+      }}
+    >
+      <button
+        onClick={changeBackground}
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '200px',
+          height: '50px',
+        }}
+      >
+        Change Background
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
